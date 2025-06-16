3bdd72b0f74de3dcdab88891ab8989ece688ca40
NishantK04 <nishantpal7510@gmail.com>
2025-06-16 23:22:40 +0530

    Add PingController for testing route mapping

---
+++ src/main/java/com/nishant/chatbot/whatsapp_chatbot/controller/PingController.java
@@ -0,0 +1,13 @@
+package com.nishant.chatbot.whatsapp_chatbot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
